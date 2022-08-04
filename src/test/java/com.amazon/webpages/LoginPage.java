8b7563eca4a0a480d6ecd08e00a600dcbc2c231a
Vinod <vkv.vinod@gmail.com>
2022-08-04 19:05:21 +0400

    login page

--- src/test/java/com.amazon/webpages/LoginPage.java
+++ src/test/java/com.amazon/webpages/LoginPage.java
@@ -2,7 +2,9 @@ package com.amazon.webpages;
 
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage {
-    private WebDriver driver;
+public class LoginPage extends BasePage{
     private static String Page_Url = "https://www.amazon.com/";
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
 }
